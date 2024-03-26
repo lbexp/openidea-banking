87bb6ceb8c69c4085dce105161cc69d538db9ab0
Fiqar Aprialim <fiqarapr@gmail.com>
2024-03-26 23:44:48 +0700

    feat: create migrations

---
+++ db/migrations/20240326154413_add_balance_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS balances (
+  balance_id UUID PRIMARY KEY NOT NULL,
+  user_id UUID NOT NULL,
+  currency VARCHAR(10) NOT NULL,
+  balance DECIMAL(12, 2) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+)
---
+++ db/migrations/20240326160717_add_transaction_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS transactions (
+  transaction_id UUID PRIMARY KEY NOT NULL,
+  user_id UUID NOT NULL,
+  balance DECIMAL(12, 2) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+)
