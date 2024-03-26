e74be9543154d7f04df5ab960a12b11f2da144b3
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-26 21:12:41 +0700

    feat : add missceleneous

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"flag"
+	"openidea-banking/src"
+
+	_ "openidea-banking/configs"
+)
+
 func main() {
 
+	var port string
+	var prefork bool
+	flag.StringVar(&port, "port", "8080", "application port")
+	flag.BoolVar(&prefork, "prefork", true, "enable prefork")
+	flag.Parse()
+
+	src.StartApplication(port, prefork)
 }
--- src/app.go
+++ src/app.go
@@ -1,5 +1,27 @@
 package src
 
-func InitApplication() {
+import (
+	"log"
+	"openidea-banking/configs"
+	"openidea-banking/middleware"
 
+	"github.com/goccy/go-json"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func StartApplication(port string, prefork bool) {
+	app := fiber.New(fiber.Config{
+		JSONEncoder:  json.Marshal,
+		JSONDecoder:  json.Unmarshal,
+		IdleTimeout:  configs.IdleTimeout,
+		WriteTimeout: configs.WriteTimeout,
+		ReadTimeout:  configs.ReadTimeout,
+		Prefork:      prefork,
+	})
+
+	app.Use(middleware.PrometheusMiddleware)
+
+	err := app.Listen(":" + port)
+	log.Fatal(err)
 }
