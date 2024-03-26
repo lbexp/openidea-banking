9b3e82343f619a5201ec2a0ec551e088dbd5218d
bayazidsustami <bayazidsustamy@gmail.com>
2024-03-26 21:17:05 +0700

    fix : resolve conflict

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-<<<<<<< HEAD
 	"openidea-banking/src"
 
 	_ "openidea-banking/configs"
@@ -12,22 +11,9 @@ func main() {
 
 	var port string
 	var prefork bool
-=======
-	app "openidea-banking/src"
-)
-
-func main() {
-	var port string
-	var prefork bool
-
->>>>>>> origin/main
 	flag.StringVar(&port, "port", "8080", "application port")
 	flag.BoolVar(&prefork, "prefork", true, "enable prefork")
 	flag.Parse()
 
-<<<<<<< HEAD
 	src.StartApplication(port, prefork)
-=======
-	app.Start(port, prefork)
->>>>>>> origin/main
 }
