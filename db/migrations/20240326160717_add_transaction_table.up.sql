7c7076204cd0afc44f13f6eb983a5ad6c9adcb94
Fiqar Aprialim <fiqarapr@gmail.com>
2024-03-28 22:37:16 +0700

    fix: adjust transaction migration

--- db/migrations/20240326160717_add_transaction_table.up.sql
+++ db/migrations/20240326160717_add_transaction_table.up.sql
@@ -1,8 +1,11 @@
 CREATE TABLE IF NOT EXISTS transactions (
   transaction_id UUID PRIMARY KEY NOT NULL,
   user_id UUID NOT NULL,
+  currency VARCHAR(10) NOT NULL,
   balance DECIMAL(12, 2) NOT NULL,
   proof_image_url VARCHAR(255),
+  bank_account_number VARCHAR(255) NOT NULL,
+  bank_name VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
 )
