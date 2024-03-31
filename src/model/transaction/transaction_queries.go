91c5b751ba4f0b14530237b49ad3ed9008206a0d
Fiqar Aprialim <fiqarapr@gmail.com>
2024-03-31 15:40:13 +0700

    fix: add order by trans

--- src/model/transaction/transaction_queries.go
+++ src/model/transaction/transaction_queries.go
@@ -5,7 +5,7 @@ import (
 )
 
 func BuildGetAllByUserIdQuery(filters GetAllByUserIdFilters) string {
-	query := "SELECT transaction_id, balance, currency, proof_image_url, created_at, bank_account_number, bank_name, COUNT(*) OVER() AS total_items FROM transactions WHERE user_id = $1"
+	query := "SELECT transaction_id, balance, currency, proof_image_url, created_at, bank_account_number, bank_name, COUNT(*) OVER() AS total_items FROM transactions WHERE user_id = $1 ORDER BY created_at DESC"
 
 	// Add limit and offset
 	if filters.Limit > 0 {
