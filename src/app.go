f3d55775cc5358f7fda46756d528effe86ba1869
Fiqar Aprialim <fiqarapr@gmail.com>
2024-04-03 21:23:20 +0700

    fix: remove timeout

--- src/app.go
+++ src/app.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"log"
 
-	"openidea-banking/src/config"
 	"openidea-banking/src/middleware"
 	"openidea-banking/src/utils"
 
@@ -14,12 +13,9 @@ import (
 
 func StartApplication(port string, prefork bool) {
 	app := fiber.New(fiber.Config{
-		JSONEncoder:  json.Marshal,
-		JSONDecoder:  json.Unmarshal,
-		IdleTimeout:  config.IdleTimeout,
-		WriteTimeout: config.WriteTimeout,
-		ReadTimeout:  config.ReadTimeout,
-		Prefork:      prefork,
+		JSONEncoder: json.Marshal,
+		JSONDecoder: json.Unmarshal,
+		Prefork:     prefork,
 	})
 
 	prome := fiberprometheus.New("openidea-bank-app")
--- src/config/fiber.go
+++
@@ -1,9 +0,0 @@
-package config
-
-import "time"
-
-const (
-	IdleTimeout  = time.Second * 5
-	WriteTimeout = time.Second * 5
-	ReadTimeout  = time.Second * 5
-)
