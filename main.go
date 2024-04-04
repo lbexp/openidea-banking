20debc922267d57510eb3074dca22d1ef1d16045
bayazidsustami <bayazidsustamy@gmail.com>
2024-04-04 21:16:21 +0700

    chore : disable prefork by default

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	var port string
 	var prefork bool
 	flag.StringVar(&port, "port", "8080", "application port")
-	flag.BoolVar(&prefork, "prefork", true, "enable prefork")
+	flag.BoolVar(&prefork, "prefork", false, "enable prefork")
 	flag.Parse()
 
 	src.StartApplication(port, prefork)
